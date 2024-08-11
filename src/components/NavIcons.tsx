64a0535cf0de06d0afef849b98f782688a4731d0
Santi <canosantiago404@gmail.com>
2024-08-11 20:04:41 -0300

    añadido iconos al navbar

--- src/components/NavIcons.tsx
+++ src/components/NavIcons.tsx
@@ -1,7 +1,67 @@
 "use client";
 
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import Link from "next/link";
+
+import CartModal from "./CartModal";
+
 const NavIcons = () => {
-  return <div>NavIcons</div>;
+  const [isProfileOpen, setIsProfileOpen] = useState(false);
+  const [isCartOpen, setIsCartOpen] = useState(false);
+
+  const router = useRouter();
+
+  // TEMP
+  const isLoggedIn = false;
+
+  const handleProfile = () => {
+    if (!isLoggedIn) {
+      router.push("/login");
+    }
+    setIsProfileOpen((prev) => !prev);
+  };
+
+  return (
+    <div className="flex items-center gap-4 xl:gap-6 relative">
+      <Image
+        src="/profile.png"
+        alt="profile"
+        width={22}
+        height={22}
+        className="cursor-pointer"
+        onClick={handleProfile}
+      />
+      {isProfileOpen && (
+        <div className="absolute p-4 rounded-md top-12 left-0 text-sm shadow-[0_3px_10px_rgb(0,0,0,0.2)] z-20">
+          <Link href="/">Perfil</Link>
+          <div className="mt-2 cursor-pointer">Cerrar sesión</div>
+        </div>
+      )}
+      <Image
+        src="/notification.png"
+        alt="notification"
+        width={22}
+        height={22}
+        className="cursor-pointer"
+      />
+      <div className="relative cursor-pointer">
+        <Image
+          src="/cart.png"
+          alt="cart"
+          width={22}
+          height={22}
+          className="cursor-pointer"
+          onClick={() => setIsCartOpen((prev) => !prev)}
+        />
+        <div className="absolute -top-4 -right-4 w-6 h-6 bg-starview rounded-full text-white text-sm flex items-center justify-center">
+          2
+        </div>
+      </div>
+      {isCartOpen && <CartModal />}
+    </div>
+  );
 };
 
 export default NavIcons;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,13 +18,19 @@ const Navbar = () => {
       {/* BIGGER SCREENS */}
       <div className="hidden md:flex items-center justify-between gap-8 h-full">
         {/* LEFT */}
-        <div className="w-1/3">
+        <div className="w-1/3 xl:w-1/2 flex items-center gap-12">
           <Link href="/" className="flex items-center gap-3">
             <Image src="/logo.webp" alt="logo" width={60} height={60} />
           </Link>
+          <div className="hidden xl:flex gap-4">
+            <Link href="/">Inicio</Link>
+            <Link href="/">Tienda</Link>
+            <Link href="/">Sobre nosotros</Link>
+            <Link href="/">Contacto</Link>
+          </div>
         </div>
         {/* RIGHT */}
-        <div className="w-2/3 flex items-center justify-between gap-8">
+        <div className="w-2/3 xl:w-1/2 flex items-center justify-between gap-8">
           <SearchBar />
           <NavIcons />
         </div>
