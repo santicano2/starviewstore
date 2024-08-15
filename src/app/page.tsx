cfad26f0947df5d0c74775da6a49735d2bd81156
Santi <canosantiago404@gmail.com>
2024-08-15 17:09:03 -0300

    fetch de productos featured

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import { Suspense } from "react";
+
 import { wixClientServer } from "@/lib/wixClientServer";
 
 import CategoryList from "@/components/CategoryList";
 import ProductList from "@/components/ProductList";
 import Slider from "@/components/Slider";
+import Skeleton from "@/components/Skeleton";
 
 const HomePage = async () => {
   //  const wixClient = await wixClientServer();
@@ -16,7 +19,12 @@ const HomePage = async () => {
       <Slider />
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl">Productos destacados</h1>
-        <ProductList />
+        <Suspense fallback={<Skeleton />}>
+          <ProductList
+            categoryId={process.env.FEATURED_PRODUCTS_CATEGORY_ID!}
+            limit={4}
+          />
+        </Suspense>
       </div>
       <div className="mt-24">
         <h1 className="text-2xl px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mb-12">
@@ -26,7 +34,7 @@ const HomePage = async () => {
       </div>
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <h1 className="text-2xl">Nuevos productos</h1>
-        <ProductList />
+        {/* <ProductList /> */}
       </div>
     </div>
   );
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,125 +1,74 @@
 import Link from "next/link";
 import Image from "next/image";
+import DOMPurify from "isomorphic-dompurify";
 
-const ProductList = () => {
+import { wixClientServer } from "@/lib/wixClientServer";
+import { products } from "@wix/stores";
+
+const PRODUCT_PER_PAGE = 20;
+
+const ProductList = async ({
+  categoryId,
+  limit,
+}: {
+  categoryId: string;
+  limit?: number;
+}) => {
+  const wixClient = await wixClientServer();
+
+  const res = await wixClient.products
+    .queryProducts()
+    .eq("collectionIds", categoryId)
+    .limit(limit || PRODUCT_PER_PAGE)
+    .find();
+
   return (
     <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
-      <Link
-        href="/test"
-        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
-      >
-        <div className="relative w-full h-80">
-          <Image
-            src="/product.png"
-            alt="product"
-            fill
-            sizes="25vw"
-            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
-          />
-          <Image
-            src="/woman.png"
-            alt="product"
-            fill
-            sizes="25vw"
-            className="absolute object-cover rounded-md"
-          />
-        </div>
-        <div className="flex justify-between">
-          <span className="font-medium">Producto</span>
-          <span className="font-semibold">$49</span>
-        </div>
-        <div className="text-sm text-gray-500">Descripción</div>
-        <button className="rounded-2xl ring-1 ring-starview text-starview w-max py-2 px-4 text-xs hover:bg-starview hover:text-white">
-          Añadir al carrito
-        </button>
-      </Link>
-      <Link
-        href="/test"
-        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
-      >
-        <div className="relative w-full h-80">
-          <Image
-            src="/product.png"
-            alt="product"
-            fill
-            sizes="25vw"
-            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
-          />
-          <Image
-            src="/woman.png"
-            alt="product"
-            fill
-            sizes="25vw"
-            className="absolute object-cover rounded-md"
-          />
-        </div>
-        <div className="flex justify-between">
-          <span className="font-medium">Producto</span>
-          <span className="font-semibold">$49</span>
-        </div>
-        <div className="text-sm text-gray-500">Descripción</div>
-        <button className="rounded-2xl ring-1 ring-starview text-starview w-max py-2 px-4 text-xs hover:bg-starview hover:text-white">
-          Añadir al carrito
-        </button>
-      </Link>
-      <Link
-        href="/test"
-        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
-      >
-        <div className="relative w-full h-80">
-          <Image
-            src="/product.png"
-            alt="product"
-            fill
-            sizes="25vw"
-            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
-          />
-          <Image
-            src="/woman.png"
-            alt="product"
-            fill
-            sizes="25vw"
-            className="absolute object-cover rounded-md"
-          />
-        </div>
-        <div className="flex justify-between">
-          <span className="font-medium">Producto</span>
-          <span className="font-semibold">$49</span>
-        </div>
-        <div className="text-sm text-gray-500">Descripción</div>
-        <button className="rounded-2xl ring-1 ring-starview text-starview w-max py-2 px-4 text-xs hover:bg-starview hover:text-white">
-          Añadir al carrito
-        </button>
-      </Link>
-      <Link
-        href="/test"
-        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
-      >
-        <div className="relative w-full h-80">
-          <Image
-            src="/product.png"
-            alt="product"
-            fill
-            sizes="25vw"
-            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
-          />
-          <Image
-            src="/woman.png"
-            alt="product"
-            fill
-            sizes="25vw"
-            className="absolute object-cover rounded-md"
-          />
-        </div>
-        <div className="flex justify-between">
-          <span className="font-medium">Producto</span>
-          <span className="font-semibold">$49</span>
-        </div>
-        <div className="text-sm text-gray-500">Descripción</div>
-        <button className="rounded-2xl ring-1 ring-starview text-starview w-max py-2 px-4 text-xs hover:bg-starview hover:text-white">
-          Añadir al carrito
-        </button>
-      </Link>
+      {res.items.map((product: products.Product) => (
+        <Link
+          href={"/" + product.slug}
+          className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+          key={product._id}
+        >
+          <div className="relative w-full h-80">
+            <Image
+              src={product.media?.mainMedia?.image?.url || "/product.png"}
+              alt="product"
+              fill
+              sizes="25vw"
+              className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+            />
+            {product.media?.items && (
+              <Image
+                src={product.media?.items[1].image?.url || "/product.png"}
+                alt="product"
+                fill
+                sizes="25vw"
+                className="absolute object-cover rounded-md"
+              />
+            )}
+          </div>
+          <div className="flex justify-between">
+            <span className="font-medium">{product.name}</span>
+            <span className="font-semibold">${product.price?.price}</span>
+          </div>
+          {product.additionalInfoSections && (
+            <div
+              className="text-sm text-gray-500"
+              dangerouslySetInnerHTML={{
+                __html: DOMPurify.sanitize(
+                  product.additionalInfoSections?.find(
+                    (section: any) => section.title === "shortDesc"
+                  )?.description || ""
+                ),
+              }}
+            ></div>
+          )}
+          <button className="rounded-2xl ring-1 ring-starview text-starview w-max py-2 px-4 text-xs hover:bg-starview hover:text-white">
+            Añadir al carrito
+          </button>
+        </Link>
+      ))}
     </div>
   );
 };
