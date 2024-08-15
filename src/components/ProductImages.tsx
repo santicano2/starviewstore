8074bcdf6bbaf56735435df1069eeaf80ad3f0b4
Santi <canosantiago404@gmail.com>
2024-08-15 17:51:03 -0300

    fetch single product

--- src/components/ProductImages.tsx
+++ src/components/ProductImages.tsx
@@ -3,33 +3,14 @@
 import { useState } from "react";
 import Image from "next/image";
 
-const images = [
-  {
-    id: 1,
-    url: "https://images.pexels.com/photos/1484807/pexels-photo-1484807.jpeg?auto=compress&cs=tinysrgb&w=400",
-  },
-  {
-    id: 2,
-    url: "https://images.pexels.com/photos/428340/pexels-photo-428340.jpeg?auto=compress&cs=tinysrgb&w=400",
-  },
-  {
-    id: 3,
-    url: "https://images.pexels.com/photos/1566412/pexels-photo-1566412.jpeg?auto=compress&cs=tinysrgb&w=400",
-  },
-  {
-    id: 4,
-    url: "https://images.pexels.com/photos/2220316/pexels-photo-2220316.jpeg?auto=compress&cs=tinysrgb&w=400",
-  },
-];
-
-const ProductImages = () => {
+const ProductImages = ({ items }: { items: any }) => {
   const [index, setIndex] = useState(0);
 
   return (
     <div>
       <div className="h-[500px] relative">
         <Image
-          src={images[index].url}
+          src={items[index].image?.url}
           alt="product"
           fill
           sizes="50vw"
@@ -37,14 +18,14 @@ const ProductImages = () => {
         />
       </div>
       <div className="flex justify-between gap-4 mt-8">
-        {images.map((image, i) => (
+        {items.map((item: any, i: number) => (
           <div
             className="w-1/4 h-32 relative gap-4 mt-8 cursor-pointer"
-            key={image.id}
+            key={item._id}
             onClick={() => setIndex(i)}
           >
             <Image
-              src={image.url}
+              src={item.image?.url}
               alt="product"
               fill
               sizes="30vw"
